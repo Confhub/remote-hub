61afade059df1642e4979222c085a8c8a79b1127
Ali Zhdanov <ali.zhdanov@oaks-lab.com>
2020-03-29 12:40:34 +0200

    Update conference card

--- src/components/ConferenceCard.tsx
+++ src/components/ConferenceCard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { formatDateRange } from "../utils/date";
 
 const Wrap = styled.div`
   background: #ffffff;
   box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.09);
   border-radius: 4px;
+`;
+
+const ImgStab = styled.div`
+  height: 100px;
+  background-color: #13131b;
+`;
+
+const Content = styled.div`
   padding: 10px;
   display: flex;
   flex-direction: column;
@@ -32,6 +41,12 @@ const LinkUnderlined = styled.a`
   font-size: 14px;
   line-height: 24px;
   color: #1890ff;
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    opacity: 0.7;
+  }
 `;
 
 const LinkButton = styled.a`
@@ -43,28 +58,47 @@ const LinkButton = styled.a`
   font-size: 14px;
   line-height: 24px;
   border-radius: 4px;
+  &:hover,
+  &:focus {
+    opacity: 0.7;
+  }
 `;
 
 export type Props = {
   title: string;
   date: string; // TODO: make it date
+  startDate: string;
+  endDate: string;
   url: string;
   ticketsUrl: string;
 };
 
-const ConferenceCard = ({ title, date, url, ticketsUrl }: Props) => {
+const ConferenceCard = ({
+  title,
+  startDate,
+  endDate,
+  url,
+  ticketsUrl
+}: Props) => {
   return (
     <Wrap>
-      <Heading>{title}</Heading>
-      <Text>{date}</Text>
-      <LinkWrap>
-        <LinkUnderlined href={url} target="_blank" rel="noopener noreferrer">
-          Visit website
-        </LinkUnderlined>
-        <LinkButton href={ticketsUrl} target="_blank" rel="noopener noreferrer">
-          Buy tickets
-        </LinkButton>
-      </LinkWrap>
+      <ImgStab />
+      <Content>
+        <Heading>{title}</Heading>
+        <Text>{formatDateRange(startDate, endDate)}</Text>
+        <LinkWrap>
+          <LinkUnderlined href={url} target="_blank" rel="noopener noreferrer">
+            Visit website
+          </LinkUnderlined>
+          <LinkButton
+            href={ticketsUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buy tickets
+          </LinkButton>
+        </LinkWrap>
+      </Content>
     </Wrap>
   );
 };
