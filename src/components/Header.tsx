2c3c163240af734d5044cec880b7d03fd0dbec24
Ali Zhdanov <ali.zhdanov@oaks-lab.com>
2020-03-29 12:44:37 +0200

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,32 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   background-color: #13131b;
-  height: 460px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 125px 50px;
+`;
+
+const Heading = styled.h1`
+  margin: 0;
+  color: #fff;
+  font-weight: 900;
+  font-size: 60px;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  color: #fff;
+  font-size: 26px;
 `;
 
 const Header = () => {
-  return <StyledHeader>Test</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Heading>Tech events around the world</Heading>
+      <Text>Join the community and find tech events all over the world</Text>
+    </StyledHeader>
+  );
 };
 
 export default Header;
