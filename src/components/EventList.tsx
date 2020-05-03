c575c07cd9d5fcd0ab0511b1cfff8b00ebdbde59
Ali Zhdanov <ali.zhdanov@oaks-lab.com>
2020-05-03 22:06:21 +0200

    Divided conferences by month

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { getMonth } from "date-fns";
 import ConferenceCard, { Props as Conference } from "./ConferenceCard";
+
+const Container = styled.div`
+  margin: 55px;
+`;
+
+const Heading = styled.h3`
+  font-size: 32px;
+`;
 
 const List = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
-  margin: 55px;
 
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
@@ -17,15 +25,47 @@ type Props = {
   conf: Conference[];
 };
 
+const MONTH_LIST = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const EventList = ({ conf }: Props) => {
+  const list = conf.reduce<Conference[][]>((acc, c) => {
+    const month = getMonth(new Date(c.startDate));
+    if (!acc[month]) {
+      acc[month] = [];
+    }
+    acc[month].push(c);
+
+    return acc;
+  }, []);
+
   return (
-    <List>
-      {conf.map((c, idx) => (
-        <li key={idx}>
-          <ConferenceCard {...c} />
-        </li>
+    <Container>
+      {list.map((item, idx) => (
+        <>
+          <Heading>{MONTH_LIST[idx]} 2019</Heading>
+          <List>
+            {item.map((c, idx) => (
+              <li key={idx}>
+                <ConferenceCard {...c} />
+              </li>
+            ))}
+          </List>
+        </>
       ))}
-    </List>
+    </Container>
   );
 };
 
